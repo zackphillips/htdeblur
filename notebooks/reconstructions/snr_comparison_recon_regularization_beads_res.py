2ae07c1ad87008495622f678c652101dfc19ebb1
sarah desktop <xsarah.dean@gmail.com>
2019-11-21 10:58:22 -0800

    cleaning up the reconstructio notebook

--- notebooks/reconstructions/snr_comparison_recon_regularization_beads_res.py
+++ notebooks/reconstructions/snr_comparison_recon_regularization_beads_res.py
@@ -30,11 +30,14 @@ def main(args):
     else:
         dataset_full_path = '/home/deansarah/Dropbox/res_line_bright_coded_raster_100_motion_deblur_2019_02_05_10_52_50'
     print(dataset_full_path)
+
+    
+    use_median_filter = ('median_filter' in args.reg_types.keys())
     # Create dataset object (loads metadata)
     if libwallerlab:
-        dataset = Dataset(dataset_full_path, use_median_filter=False, subtract_mean_dark_current=False, force_type='motion_deblur')
+        dataset = Dataset(dataset_full_path, use_median_filter=use_median_filter, subtract_mean_dark_current=False, force_type='motion_deblur')
     else:
-        dataset = MotionDeblurDataset(dataset_path=dataset_full_path, use_median_filter=False, subtract_mean_dark_current=False, force_type='')
+        dataset = MotionDeblurDataset(dataset_path=dataset_full_path, use_median_filter=use_median_filter, subtract_mean_dark_current=False, force_type='')
     force = False
 
     if libwallerlab:
@@ -48,11 +51,11 @@ def main(args):
     if args.beads:
         dataset.frame_mask = [10,11,12,13,14]
         ss = args.ss # 0.5
-        nit = 100 # 30
+        nit = args.nit # 100 # 30
     else:
         dataset.frame_mask =  [8,9,10,11]
         ss = args.ss
-        nit = 100
+        nit = args.nit # 100
 
     # Create recon object
     recon = md.recon.Reconstruction(dataset, alpha_blend_distance=1000, normalize=False, use_psf=False, estimate_background_poly=True)
@@ -66,6 +69,7 @@ def parse():
     parser.add_argument('--beads', type=int, default=1)
     parser.add_argument('--reg-types', default="{'tv': 5e-4}")
     parser.add_argument('--ss', type=float, default=1)
+    parser.add_argument('--nit', type=int, default=100)
     return parser.parse_args()
     
 if __name__ == "__main__":
